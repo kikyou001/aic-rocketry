6098930666bafab34f4bad30df46e6abb1783d42
Moughdad Mehdi Ammar <mehdiemorix@gmail.com>
2017-09-15 19:52:06 -0400

    none

--- angular/controllers/list.js
+++ angular/controllers/list.js
@@ -13,7 +13,7 @@
             {
                 university:"USTO",
                 team:"",
-                img_url:"http://placehold.it/750x950",
+                img_url:"https://lh3.googleusercontent.com/9jcSEBUpL_eDsPODnx4FOoP4VACPNVq8F4LOtPrnldW844SaZe2IZ572a7ur9_BjDQ-bMrnXK5Bq6IpCIV0E5_vyxr6oFSfXTUXRjZaSIK50hd-PBB-32rlFV3LwAXpjpMPBNdsew5ksR_GMv3l9og46tW4_de7YS3JNe_IC9lSthr0SFWS7uLM4uJooEezI9oKG09tysnwsH_QLaMfWhk0ljAbvEXAHQ2JxrJ-eUOxP1FfDvIFY4hwIGE3ELBT7dDkc_vDKTbB8ivdMo1iWr00r33J6M4hHtjAcKx-5ro9e_HL4PYdqtPF6TgmZcYj8Hys5VkvOkhPAClPS5I4XhFFFh7woDCGeR3xiRg3tpvJuUeKR1AQAnv1z47QlYxdrt4aIU7riIFYmoJF2ngikUT6r592GZIGZWiJDdukxCvN_U53--5OmlmW3Mf8ZBouE7RLhX8xI2EWAaGozt1g3uOly1a89emyJ-wh7PLzhUMU45KkoSkh01Wyt87qS4cGU8HPvGd08JVczQah6NYGxlnFjRWBKp6P-6ksyTirv2kfzzGcuDIqnqyYrQY44aGe0AlEi5yit4zgca4shWfNEz3I6dpgkml1xIr6CtoLTpTxxTRNqCY8jvvrj67qrzo7UiUI3xhbLJ2r5zk-wF0B-gRV5UOF6UPnwbgI=w497-h629-no",
                 univ_url:"http://www.univ-usto.dz",
                 rocket_name:"",
                 prof:[
@@ -30,7 +30,7 @@
             {
                 university:"UDL",
                 team:"",
-                img_url:"http://placehold.it/750x950",
+                img_url:"https://lh3.googleusercontent.com/6FzNwDBJd-8DZAn2c2isd9PcIesP_6R4CA6iPcOkPbJqEwy_KEErUrCaJloPnqYXK4XkAbzmnNFf6sqOZ9vBDllQPFbLDRviC8FrWQ8lENt5ZIBR8d7anTI-Ma85q1r7xH6MLpT8ykwgo6JRsQs5SXNjEddBQwDzW2fBSuUdgMb5ZkckCK8cDAeJ-NVgEzK7MA7go6sfS5RynJiEni-Hfo-5WR5lopuelhx4Yqopw9Q3nyKMRNzn1cUgElZHu6xd2-NJcxY1GLtkfrF3rpuDuP1flbyFa-sIaVB3lh-wGd2SNpcmnVuwRz-XDiVj9_N1OZYEf1AR4-0tidU3DGh11K7XQPMQFYoXsKFruSSHDGuI6N82r7yyuV8vUlPaUHe8slQJDyqZs9CNQJMR96f_SH4iMJ4Cw0WzG9G1z4q_h8KVFUwIFMVjwrlKHBWGdz2PvQhsylFmnghFIx5WddggNFgXGMMRiydYalkCjhW8gx4XQVztCQWIEqbMCRsw6Q2yTQ71x0yO699NYUI8zhvpZ4VhW-ZY0UtygxJRPi3J4BOQiTQkh31up8BKMtiFV2IYJG1AkQkZchD_BLzUAXaXfttWiKzlnTgAjILLh6XrfVNd-OE20mZeW5QpZYZwSSOFdb_5jAD4acCUR5WJ2Z3Dwq0_UJhcEArReB8=w497-h629-no",
                 univ_url:"https://www.univ-sba.dz",
                 rocket_name:"",
                 prof:[
@@ -43,7 +43,7 @@
             {
                 university:"Biskra",
                 team:"",
-                img_url:"http://placehold.it/750x950",
+                img_url:"https://lh3.googleusercontent.com/-FHSvIqOxmeSq_utjnCsUWGj7z9To8_JDeInYDL9JsevVFQ1tWgwGqoe__4v6RrcR3tDKchAxhH7dCGCZVeU9YuMnBSpjDGqR_2hysKMmlaHyZdjOwDTLsPYxvPOi3zQ-3fpa2_fGokA6JNAmFVodJDGm3GcIsk_Es5CwT15Wve6r6Vxh_LQcTD6PhX2kmdX-ssQn6J8QxZv-fMdksaSWRCmD_HwS8LbFYfL4wM_VyXdHGNt3FObSy-dDxGiPuNbiBsjumxizBHBiGgb04HHiwIdj8Rju6-vqye5Ng6NKaHEneMhFcZTJuXsedPEPvHGQV58zz4Kg-roHWjLLqZwLVJr0rRhtdkZnp1BCV_D3wtb9z8icZWj7iykyTNQ7cXcbE8uiC9Z8KTpp_bkmRaW_agQVo12dLvYOKVUb-BOEMp34bSXncDBvMe_hXrEG-S-2zqLFOG62xX2f46LavgztJrdCZUg1-f11AthEvfl_Huk97OfD35kdZLoiWLDT62r7-Dagx25u4r0pHyX69GQXuyM5iXD7SC1c90rPC8wEPj-rtUFl-5lhp9X18yWLCw2AdENWCrQIB7WGrCHa1EVJDpYe1slT3qsrZgMynYn4sd0H_ypLz0qQMd0IL4NG_hgpQ4QQkqTGZNG8iWAK3uSv9vVACWi1c51--c=w497-h629-no",
                 univ_url:"http://univ-biskra.dz",
                 rocket_name:"",
                 prof:[
@@ -61,7 +61,7 @@
             {
                 university:"UMC",
                 team:"",
-                img_url:"http://placehold.it/750x950",
+                img_url:"https://lh3.googleusercontent.com/Ip_geUCR5C73CMpC1G2VFqR9rA0gGiAiF1KtQNw2kqniBAlrMyihLxgomT_V8BObUiqB6_xaTiRAhm4dRf4iGGkNuRA6cwyWTGVWLcJ0tmYngD51ZG4uggk1oh1Ui9d_xVh3xF7ubkBpY5nTohrOYZRMOsfqS_BfPBpqKd5-2bWNdLMhnTXMRZeIrbtbN2tJ9i9pwPbtRLP-5zNk1cDaiw4uKLFAb_GCZGabs72DGmWgrOygFPH_DCHu7EvU5yP1kKZC9BrUmfdb3e3eYBkwxFCkAHQXD0VQcSE_QTE4owmoQ1euMjDNKKelvIZCYIGML_TZ3UFz-UxIzPkiTOAVQCOn3_ZurzX4tc9IV7e40U5ESElv8sBiNSR1veXgJYoOPWZkvLhTv_QY3QkZdkx0a94yvfFrcqJ3kBgB32Gb_RvGMmjCzmoSyTlDBhBKevCj9SFV2USfricIhe0jXuiPaYhnwUB20GYoXhpAHApXFyWNZudDzfUFPBbSUV2fCezmSEVSyZQECIaoVHcLGn-8C5js-YrYKkw_WPTKiL03V9uFV_3gk0Mw2akP5FOQ9tgYVCdMIijE0mIfSbDLwlpo3BY-XdFOFZRCV4J382SUox19dqjCKor5HUk_Qu9n34-QHe81MHB_ITUggGxU09MFm7jtzcyHf6k6zD0=w497-h629-no",
                 univ_url:"http://umc.edu.dz",
                 rocket_name:"",
                 prof:[
@@ -76,7 +76,7 @@
             {
                 university:"IAB",
                 team:"",
-                img_url:"http://placehold.it/750x950",
+                img_url:"https://lh3.googleusercontent.com/RL217l-JY2pJM3uYVyEfdoS7N6IljP3hMcdrrqF6z3xzEHJoKWghMeWyghCCaKumgzxAlqmsXCdcq3x53TciPUGHqTzjHghxlH_YUU7t1yZ3w5anIafPnOlWhypHOw65Pmy027D5lfo1CFHIefO6SL2W56Y2l4yOL8TKRpItqVM7POHLs27TRwcwqXQDp8aZoCRzu4Cst8oftbI4grjroTdLZZfGdGB1F9GpBKaciUFQURQqk0X6i_AMg9guV6fVAHMMlxaiQxBjBJnUcDXJlJDF1eroUwsnz1ZQ2LdhNpLNrTmUZSK5XEWEH9BHQnDlq6QjvRmasat_YmGX-RU3XFaEKjrm0gxtlj03RjY8Ar3eeEIgbV9jANaq848Ae9oDRh6uq69Ruv8ifzoGT9yqF155w2f7yKLC_u4qYBCVBpLGQobaOjWub7dIPKNPw2zx1sz9hws1dvQ7hnj_XdcvZtkv9tS0XrNsuhsfVOPp7DF74i01OaMnaq5ImrTGe9UKkYaggPa_hFXrgnSy9T7rZxWFyBgdqQfte27xihA1NfGiLqSEgIiOlurSpn6vVAXAjGV4mT6gwj30lnwzvcgSaTaY7SOaSTzZcYu4e2uLjsiixZRn1TzC9V4WkBfBt_uV4SHm05FhuMZQvhtvIPBbaIOJnaC42cQq6wE=w497-h629-no",
                 univ_url:"http://univ-blida.dz",
                 rocket_name:"",
                 prof:[
@@ -99,7 +99,7 @@
             {
                 university:"El-Oued",
                 team:"",
-                img_url:"http://placehold.it/750x950",
+                img_url:"https://lh3.googleusercontent.com/reBiHk-RKlgT0C1E7S8ULd5Y0egx4cS28Z84Na63z7Htq1kct4AYsRuu0aeEMe0VsRX4G46P5QV27asJqNlEfQX9aIi9vlHbMDHXQJk9fNszC6CWvgRi151qfCsKjzr3jiBCpRuOBFVMIXvHbK8Q7a9MIi4kkCVk-JTCi86KBQ6Ocs8u7QKv1uQmLflPgqyM5W7-4nmeZ9F4LSi8HNPYQENM9j7lffDBXRw6J4addb_CYfSsBNzLcy3MW5eJYq3Sci6aVLoVj5zSguFLIZXsP9sp8KVQs5JeKoa0JGWQKXZ3XL2eI1-eILSdhU5gMZNa4-QXOacGLtdV94sxpAphskR1gvfjNcPeZo4BujycXBHr923JBpgdoHCnbzo6Ro4p9RqfLh-HJQH34MD8_fTUO2X0YAgSB5lopfD6tV-6n8DIjF8t8FY9NM4zfT9kItsgXvtP_PdLTF6xjDkdslhvTIZSzTeilqVLIkbicPoUFy-8-jBXB9KhvAalIFtDAtEW8iXq527aryu1pOPaVY3i-5pgh4EXXVT8I9u6yjCIMNVvZpBOH7wChNBhEMDt6yLjZyECowBF8IHHgX_s55_nmoZbTE4PNx2lmqkfl1FOzxotnrfYhgNtZMekV8-_Gj3gdAV_BVn3Q5MlgL4pvsEkIcLCCOIc75Bqd-Q=w497-h629-no",
                 univ_url:"http://univ-eloued.dz",
                 rocket_name:"",
                 prof:[
@@ -114,7 +114,7 @@
             {
                 university:"USTHB",
                 team:"",
-                img_url:"http://placehold.it/750x950",
+                img_url:"https://lh3.googleusercontent.com/I3oFLBjBQrFFRK66RqEjp8hRcaX63CGptI8yPO-tnxn3dm_vpSqxndKRyadBvkPWGgSdi3cvTuQ7d80YNVhgti5njPhGwSXVGIkmDBMH0BmIggsXnBbrF-cRBPtIq0-K_RGvaEiDm3oGorY_E8Q_kgtxReKOMCkauaTXaKS4rAO4IjGCCdGbwKjRhDvS1A0_mrX5d6evHfHPqHwng8EZXsnAw7V0uFB7a1dDyGRgQLR7wo0YOIwjUbpirL9b8cWpF6E9Hrrzouj14mWjnx4meTxzkn535p-OWsp0pi9GJZIcMZ9UVWyqzBW6-bVxjgF77ozLii60uWehbXDGSN84AJ7ieZaGyDrM3hHoUdtFL9d3u5zBDeyEFxutwCJWvQx30i1V3vykkQ1i7mv3SPOBo3v6yn7k9pwOF3_0rvqicQiSMDpjb-aizi1UX10nrlOoKnIO1geY0DkVIltPm6efeaYX8ud6mX6EQeQeZ2VyvCUZtjhGD4m0cZb0QeZS8KdIVAf1yRKqqvZpdAKXd9Ceds1_972FA1zspZulZHgSro03J0Fflclr2s_Z2fqm_5ex_g81WiCJuuq9lY-6Ho8bteyGFxI6dbQvRTOlQnlRBtKV1zMk_-edQx7kI9x5Re3GkBlpwFjRl7Eno53i85wP7ceXWc5OZ3cg24g=w497-h629-no",
                 univ_url:"http://usthb.dz",
                 rocket_name:"",
                 prof:[
@@ -131,7 +131,7 @@
             {
                 university:"ENP-team1",
                 team:"",
-                img_url:"http://placehold.it/750x950",
+                img_url:"https://lh3.googleusercontent.com/sTwguU97YOngUn5h4M400m-lnxcCoFuMbItAmuiqOKMS4GW7h-UbQ8Wk_9TmYeEJ62I7cdF-3d3GjF_-rVwPbEZtfhyxePiqKdJBTmDEK-_amOlbTR5zGu9PHuuRtkA4NubiuwPEK7H13X7gQ2n3YRjy4rQjYLbTlPbfVS_4clDNjTzaPAhh5D8tDbRl3HF_NWYi1tICRx739Xl65uWPun_j_JGob1VVHm4J80LnYHt86tMZaHhxjIiW_ht-op7AIcb_r5kyZMcT33Dj_sEuurlpyHV88ARn7xl0FKuLFd_ocw2NFjv6RVA9GLVtXaJfqqMn29fcHi6EMOVzTKX0cfI17B4GHuWoPjWYn-sMsmf666WI06OBnFDBu7tS9_TmxG1rahhIWqlUKgbH5mpbZPD03O0olW9r1MY3JyJcAjRCKQc7dqjPKiEOVZ0gnOOt2NxqMH0CNSDgFpWOv1iIXCNP0kIULrOUJYn3WnNwZjpHAWcjR9-QgfDwClG3foe2bCvj1NRdIBGZaoq4voot0jcIDjOWTr3ZA5nIcrFx7_nvS6bW-VjNLUcwTKoIjt8dQ_LpBtDcmvTQff7TIJrJ6qJS-aUXulW_4NSpOXzs6Gt1s2XkHV0UouYr55XmM6Fm0ynFRD0-1VKvDi6Bd4pQeQbsT2uaF0Hxu-g=w497-h629-no",
                 univ_url:"http://enp.edu.dz",
                 rocket_name:"",
                 prof:[
@@ -162,7 +162,7 @@
             {
                 university:"ENP-team2",
                 team:"",
-                img_url:"http://placehold.it/750x950",
+                img_url:"https://lh3.googleusercontent.com/sTwguU97YOngUn5h4M400m-lnxcCoFuMbItAmuiqOKMS4GW7h-UbQ8Wk_9TmYeEJ62I7cdF-3d3GjF_-rVwPbEZtfhyxePiqKdJBTmDEK-_amOlbTR5zGu9PHuuRtkA4NubiuwPEK7H13X7gQ2n3YRjy4rQjYLbTlPbfVS_4clDNjTzaPAhh5D8tDbRl3HF_NWYi1tICRx739Xl65uWPun_j_JGob1VVHm4J80LnYHt86tMZaHhxjIiW_ht-op7AIcb_r5kyZMcT33Dj_sEuurlpyHV88ARn7xl0FKuLFd_ocw2NFjv6RVA9GLVtXaJfqqMn29fcHi6EMOVzTKX0cfI17B4GHuWoPjWYn-sMsmf666WI06OBnFDBu7tS9_TmxG1rahhIWqlUKgbH5mpbZPD03O0olW9r1MY3JyJcAjRCKQc7dqjPKiEOVZ0gnOOt2NxqMH0CNSDgFpWOv1iIXCNP0kIULrOUJYn3WnNwZjpHAWcjR9-QgfDwClG3foe2bCvj1NRdIBGZaoq4voot0jcIDjOWTr3ZA5nIcrFx7_nvS6bW-VjNLUcwTKoIjt8dQ_LpBtDcmvTQff7TIJrJ6qJS-aUXulW_4NSpOXzs6Gt1s2XkHV0UouYr55XmM6Fm0ynFRD0-1VKvDi6Bd4pQeQbsT2uaF0Hxu-g=w497-h629-no",
                 univ_url:"http://enp.edu.dz",
                 rocket_name:"",
                 prof:[
